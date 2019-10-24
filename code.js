5c8f3068d0f45627ba03697dc05373e1304ec70a
Paul <isakura313@gmail.com>
2019-10-24 11:11:36 +0300

    all

---
+++ code.js
@@ -0,0 +1,9 @@
+let root = document.getElementById('root');
+let arrow_left = document.querySelector('.arrow-left');
+let arrow_right = document.querySelector('.arrow-right');
+
+function changePhoto(){
+
+}
+
+arrow_right.addEventListener('click', changePhoto);
