3daf0ba83a59e5a75c98a6f1b92a94382a87dcf8
Paul <isakura313@gmail.com>
2019-10-24 11:22:45 +0300

    ok

--- code.js
+++ code.js
@@ -2,8 +2,16 @@ let root = document.getElementById('root');
 let arrow_left = document.querySelector('.arrow-left');
 let arrow_right = document.querySelector('.arrow-right');
 
-function changePhoto(){
 
+
+let i = 1;
+root.style.backgroundImage = `url('img/${i}.jpeg')`;
+function changePhoto(){
+    ++i;
+    root.style.backgroundImage = `url('img/${i}.jpeg')`;
+    if(i == 3){
+        i = 0;
+    }
 }
 
 arrow_right.addEventListener('click', changePhoto);
